143e8bf05cc2f2006e72f6a6051aef1b0d13a172
moon18004 <purnsup90@gmail.com>
2021-11-27 02:30:02 -0700

    111

--- assignments/lesson11/scripts/center.js
+++ assignments/lesson11/scripts/center.js
@@ -1,3 +1,43 @@
+let date = new Date();
+
+let daysInWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+let months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+document.querySelector('.date').textContent =
+  daysInWeek[date.getDay()] +
+  ', ' +
+  String(date.getDate()) +
+  ' ' +
+  months[date.getMonth()] +
+  ' ' +
+  String(date.getFullYear());
+
+  function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
 function adjustRating(rating) {
     document.querySelector("#rating_value").innerHTML = rating;
   }
--- assignments/lesson11/scripts/main.js
+++ assignments/lesson11/scripts/main.js
@@ -5,6 +5,45 @@
 
 // document.getElementById("lastupdated").textContent =
 //   "Last Updated: " + document.lastModified;
+let date = new Date();
+
+let daysInWeek = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+let months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+document.querySelector('.date').textContent =
+  daysInWeek[date.getDay()] +
+  ', ' +
+  String(date.getDate()) +
+  ' ' +
+  months[date.getMonth()] +
+  ' ' +
+  String(date.getFullYear());
+
+  function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
 
 
 const navbarMenu = document.querySelector('.navbar__menu');
