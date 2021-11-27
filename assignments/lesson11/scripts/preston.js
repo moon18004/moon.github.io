12e7dd40ba5cd0a193a91babd553a5cc90bbec8d
moon18004 <purnsup90@gmail.com>
2021-11-27 02:17:47 -0700

    11

---
+++ assignments/lesson11/scripts/preston.js
@@ -0,0 +1,55 @@
+// let date = new Date();
+
+// let daysInWeek = [
+//   'Sunday',
+//   'Monday',
+//   'Tuesday',
+//   'Wednesday',
+//   'Thursday',
+//   'Friday',
+//   'Saturday',
+// ];
+
+// let months = [
+//   'January',
+//   'February',
+//   'March',
+//   'April',
+//   'May',
+//   'June',
+//   'July',
+//   'August',
+//   'September',
+//   'October',
+//   'November',
+//   'December',
+// ];
+
+// document.querySelector('.date').textContent =
+//   daysInWeek[date.getDay()] +
+//   ', ' +
+//   String(date.getDate()) +
+//   ' ' +
+//   months[date.getMonth()] +
+//   ' ' +
+//   String(date.getFullYear());
+
+//   function toggleMenu() {
+//     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+// }
+
+const navbarMenu = document.querySelector('.navbar__menu');
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () =>{
+    navbarMenu.classList.toggle('open');
+})
+
+if (date.getDay() == 5){
+  document.querySelector('#banner').style.display = 'block';
+}
+
+WebFont.load({
+  google: {
+    families: ['Architects Daughter', 'Indie Flower'],
+  },
+});
